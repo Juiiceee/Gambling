535e58b0d9f273f2c15eaefa6f401e4c053e2997
Juiceee <lbehr@student.42.fr>
2024-06-04 12:24:51 +0200

    semicolon + test constructor

--- src/Gambling.sol
+++ src/Gambling.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier:MIT
 
-pragma solidity ^0.8.2
+pragma solidity ^0.8.2;
 
 contract Gambling{
 	mapping(address => uint) public nbTicketsByAddress;
 	uint public nbTickets;
-
-	constructor {
-		
+	uint public salut;
+	constructor (uint oui) {
+		salut = oui;
 	}
 }
